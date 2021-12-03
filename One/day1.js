1171138e21343ea2e46ad801b82b3da76a3799cb
MW <stompy2000@protonmail.com>
2021-12-03 16:21:18 +0000

    day 1 set up

---
+++ One/day1.js
@@ -0,0 +1,11 @@
+
+const day1 = () => {
+  console.time('codezup')
+
+  
+
+  console.timeEnd('codezup')
+  return "day 1"
+}
+
+module.exports = day1;
---
+++ One/day1.test.js
@@ -0,0 +1,8 @@
+const day1 = require('./day1')
+
+describe('day1', () => {
+    test('should pass', () => {
+        day1()
+        expect(true).toBeTruthy()
+    })
+});
