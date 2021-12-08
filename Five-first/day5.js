bd66bf22bd8d08e499674abb37fe7d3818369323
MW <stompy2000@protonmail.com>
2021-12-08 18:19:13 +0000

    day5 line get direction

--- Five-first/day5.js
+++ Five-first/day5.js
@@ -16,10 +16,55 @@ const createGrid = (rawData) => {
   return grid
 }
 
+const getCoordinates = (rawData) => {
+  return rawData.split('\n').map(row => row.split(' -> ').map(pos => pos.split(',').map(Number)))
+}
+
+const getDirection = (x, y) => {
+  if(x == 0 && y > 0) return 'DOWN'
+  if(x == 0 && y < 0) return 'UP'
+  if(y == 0 && x > 0) return 'RIGHT'
+  if(y == 0 && x < 0) return 'LEFT'
+}
+
+const markCoordinates = (coordinates, grid) => {
+  const markedGrid = [...grid]
+
+  coordinates.forEach(posPair => {
+    const start = posPair[0]
+    const end = posPair[1]
+    const distanceX = end[0] - start[0]
+    const distanceY = end[1] - start[1]
+    const direction = getDirection(distanceX, distanceY)
+    switch (direction) {
+      case "DOWN":
+        //down
+        break;
+      case "UP":
+        //up
+        break;
+      case "LEFT":
+        //left
+        break;
+      case "RIGHT":
+        //right
+        break;
+      default:
+        break;
+    }
+
+  });
+
+
+  return markedGrid
+}
+
 const day5 = (dataPath) => {
   console.time('day5-1')
   const rawData = readFile(dataPath)
   const grid = createGrid(rawData)
+  const coordinates = getCoordinates(rawData)
+  console.log('coordinates', coordinates)
 
 
 
