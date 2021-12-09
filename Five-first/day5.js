8c2e79cb2c6be4f7cce87a0cdb00c67eeddec922
MW <stompy2000@protonmail.com>
2021-12-09 13:51:33 +0000

    day5 pt 1 complete

--- Five-first/day5.js
+++ Five-first/day5.js
@@ -1,14 +1,8 @@
 const readFile = require("../utils/readFile")
 
-const createGrid = (rawData) => {
-  const xVals = rawData.split(/\n| -> /).map(pos => parseInt(pos[0]))
-  console.log('xVals', xVals)
-  const yVals = rawData.split(/\n| -> /).map(pos => parseInt(pos[2]))
-  console.log('yVals', yVals)
-  const biggestX = Math.max(...xVals)
-  console.log('biggestX', biggestX)
-  const biggestY = Math.max(...yVals)
-  console.log('biggestY', biggestY)
+const createGrid = () => {
+  const biggestX = 999
+  const biggestY = 999
 
   const grid = []
   for(row = 0; row <= biggestX; row++) {
@@ -17,7 +11,6 @@ const createGrid = (rawData) => {
       grid[row][col] = 0
     }
   }
-  console.log('grid', grid)
   return grid
 }
 
@@ -35,9 +28,8 @@ const getDirection = (x, y) => {
 
 const markCoordinates = (coordinates, grid) => {
   const markedGrid = [...grid]
-  console.table(markedGrid)
-
-  coordinates.forEach(posPair => {
+  coordinates.pop() // remove single NaN from data, can't work out why it's there
+  coordinates.forEach((posPair, index) => {
     const start = posPair[0]
     const end = posPair[1]
     const distanceX = end[0] - start[0]
@@ -45,7 +37,6 @@ const markCoordinates = (coordinates, grid) => {
     const direction = getDirection(distanceX, distanceY)
     switch (direction) {
       case "DOWN":
-        console.log("DOWN") 
         for(let y = start[1]; y <= end[1]; y++) {
           markedGrid[y][start[0]]++
         }
@@ -88,10 +79,8 @@ const countIntersections = (grid) => {
 const day5 = (dataPath) => {
   console.time('day5-1')
   const rawData = readFile(dataPath)
-  const grid = createGrid(rawData)
-  console.log('grid', grid)
+  const grid = createGrid()
   const coordinates = getCoordinates(rawData)
-  console.log('coordinates', coordinates)
   const markedGrid = markCoordinates(coordinates ,grid)
   const intersections = countIntersections(markedGrid)
   console.timeEnd('day5-1')
--- Five-first/day5.test.js
+++ Five-first/day5.test.js
@@ -1,11 +1,11 @@
 const day5 = require('./day5')
 
-// test('should have same value as example', () => {
-//   expect(day5('./Five-first/example.txt')).toBe(5)
-// })
+test('should have same value as example', () => {
+  expect(day5('./Five-first/example.txt')).toBe(5)
+})
 
 test('should have same value as input', () => {
-  expect(day5('./Five-first/input.txt')).toBe(45031)
+  expect(day5('./Five-first/input.txt')).toBe(8060)
 })
 
 
